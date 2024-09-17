5093a3b0b9394c523f220cf55b54161f36fd1b78
LeoMulinari <leo.mulinari@gmail.com>
2024-09-17 04:44:20 -0300

    Listagem de MinhasSolicitacoes

--- src/components/TelasSolicitante/MinhasSolicitacoes.js
+++ src/components/TelasSolicitante/MinhasSolicitacoes.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import styles from './TelasSolicitante.module.css';
-import Cabecalho from '../Cabecalho/Cabecalho';
+import { format } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import api from '../../api/api';
 import ActionBar from '../ActionBar/ActionBar';
+import Cabecalho from '../Cabecalho/Cabecalho';
 import MinhasSolicitacoesItem from './MinhasSolicitacoesItem';
+import styles from './TelasSolicitante.module.css';
 
 function MinhasSolicitacoes() {
-    const solicitacoes = [
+    /*const solicitacoes = [
         {
             numProtocolo: "21009826",
             dataAbertura: "29/03/2022",
@@ -54,14 +56,29 @@ function MinhasSolicitacoes() {
             descricao: "Laptop com processador Intel i5, 8GB RAM, 256GB SSD.",
             imageUrl: "/imagens/ordem.svg"
         },
-    ];
+    ];*/
 
+    const [solicitacoes, setSolicitacoes] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [searchTerm, setSearchTerm] = useState('');
     const itemsPerPage = 5;
 
+    const solicitanteId = 6;
+
+    useEffect(() => {
+        const fetchSolicitacoes = async () => {
+            try {
+                const response = await api.get(`/osmenu/solicitante/${solicitanteId}`);
+                setSolicitacoes(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar ordens de serviço:', error);
+            }
+        };
+        fetchSolicitacoes();
+    }, [solicitanteId]);
+
     const filteredSolicitacoes = solicitacoes.filter(solicitacao =>
-        solicitacao.numProtocolo.toLowerCase().includes(searchTerm.toLowerCase())
+        solicitacao.id.toString().toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     const totalPages = Math.ceil(filteredSolicitacoes.length / itemsPerPage);
@@ -88,7 +105,12 @@ function MinhasSolicitacoes() {
                 <h2 className={styles.listTitle}>Minhas Solicitações</h2>
                 <section className={styles.listSection}>
                     {currentItems.map((solicitacao, index) => (
-                        <MinhasSolicitacoesItem key={index} {...solicitacao} />
+                        <MinhasSolicitacoesItem key={index} id={solicitacao.id}
+                            data_abertura={format(solicitacao.data_abertura, "dd/MM/yyyy")}
+                            data_finalizacao={format(solicitacao.data_finalizacao, "dd/MM/yyyy")}
+                            descricao={solicitacao.descricao}
+                            status={solicitacao.status}
+                        />
                     ))}
                 </section>
             </div>
