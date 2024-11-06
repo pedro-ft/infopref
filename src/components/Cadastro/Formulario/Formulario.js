db251794c4b4a25af44d1d0069e17ac7b2def57e
LeoMulinari <leo.mulinari@gmail.com>
2024-11-06 20:17:32 -0300

    Formato IP ajustado

--- src/components/Cadastro/Formulario/Formulario.js
+++ src/components/Cadastro/Formulario/Formulario.js
@@ -5,7 +5,14 @@ import styles from './Formulario.module.css';
 const Formulario = ({ campos, onSubmit, voltarUrl, mostrarRequisitosSenha }) => {
   const [formData, setFormData] = useState({});
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [ipError, setIpError] = useState(null);
   const navigate = useNavigate();
+
+  // Função para validar o IP
+  const isValidIP = (ip) => {
+    const ipPattern = /^(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})){3}$/;
+    return ipPattern.test(ip);
+  };
 
   const formatPhoneNumber = (value) => {
     let formattedValue = value.replace(/\D/g, '');
@@ -39,11 +46,23 @@ const Formulario = ({ campos, onSubmit, voltarUrl, mostrarRequisitosSenha }) => 
       formattedValue = formatRemoteId(value);
     }
 
+    if (name === 'ip') {
+      if (isValidIP(value)) {
+        setIpError(null);
+      } else {
+        setIpError('Por favor, insira um IP válido. Exemplo: 192.168.0.1');
+      }
+    }
+
     setFormData({ ...formData, [name]: formattedValue });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (ipError) {
+      alert('Por favor, corrija os erros antes de enviar.');
+      return;
+    }
     await onSubmit(formData);
     navigate(voltarUrl);
   };
@@ -95,10 +114,14 @@ const Formulario = ({ campos, onSubmit, voltarUrl, mostrarRequisitosSenha }) => 
                   value={formData[campo.name] || ''}
                   onChange={handleInputChange}
                 />
+                {campo.name === 'ip' && ipError && (
+                  <p className={styles.errorText}>{ipError}</p>
+                )}
               </div>
             )}
           </div>
         ))}
+
         {mostrarRequisitosSenha && (
           <p className={styles.passwordRequirements}>
             A senha deve ter entre 5 e 20 caracteres, incluindo pelo menos uma letra e um número.
